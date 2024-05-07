95706cd28e1a1fc67790eacf78bd69b6f8437add
amadou-toure <amadouelcapotoure@gmail.com>
2024-05-06 21:01:08 -0400

    player layout update

--- music_player/groove-main/src/Player.jsx
+++ music_player/groove-main/src/Player.jsx
@@ -6,7 +6,9 @@ import no_artwork from "../assets/images/no_artwork.png";
 
 export default function Player({ navigation, route }) {
 	const { Artwork, Title, Artist } = route.params;
-	const button_size = 30;
+	const button_size = 32;
+	const [isPlaying, setIsPlaying] = React.useState(false);
+	const [isLiked, setIsLiked] = React.useState(false);
 	return (
 		<View style={styles.Player}>
 			<View style={styles.Track_info}>
@@ -21,13 +23,44 @@ export default function Player({ navigation, route }) {
 						source={no_artwork}
 					/>
 				)}
-				<View>
-					<Text style={styles.Primary_text}>{Title}</Text>
-					{Artist ? (
-						<Text style={styles.Secondary_text}>{Artist}</Text>
-					) : (
-						<Text style={styles.Secondary_text}>Unknown</Text>
-					)}
+				<View
+					style={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between",
+						alignItems: "center",
+						width: "80%",
+						flex: 0.3,
+					}}
+				>
+					<View
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "space-between",
+							alignItems: "left",
+							height: "50%",
+							width: "80%",
+						}}
+					>
+						<Text style={styles.Primary_text}>{Title}</Text>
+						{Artist ? (
+							<Text style={styles.Secondary_text}>{Artist}</Text>
+						) : (
+							<Text style={styles.Secondary_text}>Unknown</Text>
+						)}
+					</View>
+					<Pressable
+						onPress={() => {
+							isLiked ? setIsLiked(false) : setIsLiked(true);
+						}}
+					>
+						<Ionicons
+							name={isLiked ? "heart" : "heart-outline"}
+							size={button_size}
+							color={Main_color.Secondary_color}
+						/>
+					</Pressable>
 				</View>
 			</View>
 			<View style={styles.Player_control}>
@@ -51,10 +84,12 @@ export default function Player({ navigation, route }) {
 						borderRadius: 80,
 						padding: 20,
 					}}
-					onPress={() => console.log("play")}
+					onPress={() => {
+						isPlaying ? setIsPlaying(false) : setIsPlaying(true);
+					}}
 				>
 					<Ionicons
-						name="play-outline"
+						name={isPlaying ? "pause" : "play"}
 						size={button_size}
 						color={Main_color.Secondary_color}
 					/>
